c7a43b84ed16dec3ba8fd1facc1b5289b8462f33
shelleywang <shelleyw13@gmail.com>
2015-07-10 15:11:29 -0700

    Created asteroid location generator function

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,13 @@
-// start slingin' some d3 here.
-
+// function to create a set of asteroids
+var generateLocations = function(numAsteroids) {
+  var locations = [];
+  for (var i = 0; i < numAsteroids; i ++) {
+    var location = {};
+    var body = d3.select("body");
+    location.x = +d3.select('body').style('width').slice(0, -2) * Math.random();
+    location.y = +d3.select('body').style('height').slice(0, -2) * Math.random();
+    debugger;
+    locations.push(location);
+  }
+  return locations;
+};
