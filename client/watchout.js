66886efa4e8256757604b037dffd9e7b3dfb4398
shelleywang <shelleyw13@gmail.com>
2015-07-10 15:45:55 -0700

    Placed asteroid elements onto body

--- client/watchout.js
+++ client/watchout.js
@@ -6,8 +6,13 @@ var generateLocations = function(numAsteroids) {
     var body = d3.select("body");
     location.x = +d3.select('body').style('width').slice(0, -2) * Math.random();
     location.y = +d3.select('body').style('height').slice(0, -2) * Math.random();
-    debugger;
     locations.push(location);
   }
   return locations;
 };
+
+var asteroids = d3.select('body').selectAll('.asteroids');
+asteroids.data(generateLocations(10)).enter().append('div')
+    .classed('asteroid', true)
+    .style('left', function(d, i) { return d.x + 'px'; })
+    .style('top', function(d) { return d.y + 'px';});
