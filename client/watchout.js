85e232119d8e44ac23f68aa80959886805d777ed
shelleywang <shelleyw13@gmail.com>
2015-07-10 16:11:43 -0700

    Added SVG element to body and asteroids to the SVG element

--- client/watchout.js
+++ client/watchout.js
@@ -4,15 +4,24 @@ var generateLocations = function(numAsteroids) {
   for (var i = 0; i < numAsteroids; i ++) {
     var location = {};
     var body = d3.select("body");
-    location.x = +d3.select('body').style('width').slice(0, -2) * Math.random();
-    location.y = +d3.select('body').style('height').slice(0, -2) * Math.random();
+    location.x = (+d3.select('body').style('width').slice(0, -2) - 175) * Math.random() + 75; 
+    location.y = (+d3.select('body').style('height').slice(0, -2) - 175) * Math.random() + 70;
     locations.push(location);
   }
   return locations;
 };
 
-var asteroids = d3.select('body').selectAll('.asteroids');
-asteroids.data(generateLocations(10)).enter().append('div')
+// adds SVG element to the body
+var svg = d3.select("body").append("svg")
+    .attr("width", '100%')
+    .attr("height", '90%')
+  .append("g");
+
+// places asteroids onto the body
+//var asteroids = d3.select('body').selectAll('.asteroids');
+svg.selectAll('circle').data(generateLocations(10)).enter().append('circle')
     .classed('asteroid', true)
-    .style('left', function(d, i) { return d.x + 'px'; })
-    .style('top', function(d) { return d.y + 'px';});
+    .attr('cx', function(d, i) { return d.x + 'px'; })
+    .attr('cy', function(d) { return d.y + 'px';})
+    .attr('r', 20);
+
