bea0f90b0e5b47b7238c4b74289622ae0d837fbb
shelleywang <shelleyw13@gmail.com>
2015-07-11 15:00:33 -0700

    Changed <circle>s to <image>s for player and asteroids

--- client/watchout.js
+++ client/watchout.js
@@ -26,45 +26,16 @@ var svg = d3.select("body").append("svg")
     .attr("height", '90%')
   .append("g");
 
-
-
-// STYLING JELLYFISH TEST
-
-// var defs= svg.append('defs')
-// defs.append('pattern')
-//     .attr('id', 'fish')
-//     .attr('patternUnits', 'userSpaceOnUse')
-//     .attr('width', 160) //160
-//     .attr('height', 150) //150
-//   .append('svg:image')
-//     .attr('xlink:href', 'http://orig11.deviantart.net/d50e/f/2012/356/0/0/jellyfish_icon_by_piirustus-d5ost8i.gif')
-//     .attr("width", 70) //150
-//     .attr("height", 70) //150
-//     .attr('x',65) //20
-//     .attr('y',65); //20
-
-
-
-// svg.append("circle")
-//     .attr("r",'60') //75
-//     .attr('cx',100)
-//     .attr('cy',100)
-//     .attr('stroke',"black") 
-//     .attr('stroke-width',"3")
-//     .attr("fill", "url(#fish)");
-
-// TEST
-
 // places asteroids onto the body
-//var asteroids = d3.select('body').selectAll('.asteroids');
 var generateAsteroids = function() {
-  svg.selectAll('circle').data(generateLocations())
-    .enter().append('circle')
+  svg.selectAll('.asteroid').data(generateLocations())
+    .enter().append('image')
     .classed('asteroid', true)
-    .attr('cx', function(d, i) { return d.x + 'px'; })
-    .attr('cy', function(d) { return d.y + 'px';})
-    .attr('r', 20)
-    .attr('fill','white');
+    .attr('x', function(d, i) { return d.x + 'px'; })
+    .attr('y', function(d) { return d.y + 'px';})
+    .attr('xlink:href', 'jellyfish.gif')
+    .attr('height',80)
+    .attr('width',80);
 };
 
 
@@ -72,8 +43,8 @@ var moveAsteroids = function() {
   var asteroids = d3.selectAll('.asteroid');
   asteroids.data(generateLocations());
   asteroids.transition().duration(2000)
-    .attr('cx', function(d, i) { return d.x + 'px'; })
-    .attr('cy', function(d) { return d.y + 'px';});
+    .attr('x', function(d, i) { return d.x + 'px'; })
+    .attr('y', function(d) { return d.y + 'px';});
 };
 
 
@@ -84,43 +55,16 @@ setInterval(function() {moveAsteroids();},2100);
 
 // PLAYER STUFF
 
-// TEST
-
-var defs= svg.append('defs')
-defs.append('pattern')
-    .attr('id', 'fish')
-    .attr('patternUnits', 'userSpaceOnUse')
-    .attr('width', 150) //160
-    .attr('height', 150) //150
-    .attr('x',-50)
-    .attr('y',-50)
-  .append('svg:image')
-    .attr('xlink:href', 'https://38.media.tumblr.com/adb56cc7e2a2fbeaec8dfef1d409b543/tumblr_nn27cqMKDY1u4vod5o1_500.gif')
-    .attr("width", 70) //150
-    .attr("height", 70) //150
-    .attr('x',15) //20
-    .attr('y',15); //20
-
-// svg.append("circle")
-//     .attr("r",'30') //75
-//     .attr('cx',100)
-//     .attr('cy',100)
-//     .attr('stroke',"black") 
-//     .attr('stroke-width',"3")
-//     .attr("fill", "url(#fish)");
-
-// TEST
-
-
 var generatePlayer = function() {
   svg.selectAll('.player').data([generateRandomLocation()])
-    .enter().append('circle')
+    .enter().append('image')
     .classed('player', true)
     .attr("transform", function(d){return 'translate(' + d.x + ',' + d.y + ')'})
-    .attr('r', 30)
-    .attr('cx',0)
-    .attr('cy',0)
-    .attr("fill", "url(#fish)");
+    .attr('xlink:href', 'fish.gif')
+    .attr('height',100)
+    .attr('width',100)
+    .attr('x', -50)
+    .attr('y', -50);
 };
 
 var drag = d3.behavior.drag().on("drag", dragmove);
@@ -144,8 +88,8 @@ window.currentScore = 0;
 var findCollisions = function() {
   var asteroids = d3.selectAll('.asteroid');
   //find centerpoint of player
-  var playerX = d3.transform(d3.select('.player').attr("transform")).translate[0]; 
-  var playerY = d3.transform(d3.select('.player').attr("transform")).translate[1];
+  var playerX = d3.transform(d3.select('.player').attr("transform")).translate[0] ; 
+  var playerY = d3.transform(d3.select('.player').attr("transform")).translate[1] ;
 
   var numCollisions = 0;
   // loop over array of asteroids
@@ -156,9 +100,9 @@ var findCollisions = function() {
 
     // check if player is overlapping with asteroid
     // absolute value of difference between player.x and asteroid.x < radii 
-    var overlappingX = Math.abs(playerX - asteroidX) < 35;
+    var overlappingX = Math.abs(playerX - asteroidX) < 60;
     // absolute value of difference between player.y and asteroid.y < radii
-    var overlappingY = Math.abs(playerY - asteroidY) < 35;
+    var overlappingY = Math.abs(playerY - asteroidY) < 60;
     
     if (overlappingX && overlappingY) {
       numCollisions = 1;
